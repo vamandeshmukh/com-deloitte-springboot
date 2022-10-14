3549ae3d80eba543887c1bf47301a4aa370a2d1a
Vaman Deshmukh <vamandeshmukh@gmail.com>
2022-10-14 14:57:46 +0530

    spring boot

--- src/main/java/com/deloitte/springboot/demo/controller/HelloController.java
+++ src/main/java/com/deloitte/springboot/demo/controller/HelloController.java
@@ -6,10 +6,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
+//	http://localhost:8080/hello
+
 	@RequestMapping("/hello")
 	public String hello() {
 		System.out.println("hello");
 		return "Hello world!";
+	}
+
+//	http://localhost:8080/hi
+
+	@RequestMapping("/hi")
+	public String hi() {
+		System.out.println("hi");
+		return "Hi! How're you?";
 	}
 
 }
