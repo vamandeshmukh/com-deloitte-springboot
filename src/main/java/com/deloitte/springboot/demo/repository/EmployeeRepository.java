b7ee3bd83c13e1b3bd1f1cadb5b0452d96132172
Vaman Deshmukh <vamandeshmukh@gmail.com>
2022-10-17 14:39:38 +0530

    spring-boot

--- src/main/java/com/deloitte/springboot/demo/repository/EmployeeRepository.java
+++ src/main/java/com/deloitte/springboot/demo/repository/EmployeeRepository.java
@@ -1,8 +1,25 @@
 package com.deloitte.springboot.demo.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.deloitte.springboot.demo.model.Employee;
+
 @Repository
-public interface EmployeeRepository {
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+//	basic CRUD Ops -
+	// no need to write any methods
+	
+	// findById();
+	// findAll();
+	// save();
+	// save();
+	// deleteById();
+	
+	
+	
+	
+	
 
 }
--- src/main/java/com/deloitte/springboot/demo/service/EmployeeService.java
+++ src/main/java/com/deloitte/springboot/demo/service/EmployeeService.java
@@ -1,28 +1,30 @@
 package com.deloitte.springboot.demo.service;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.deloitte.springboot.demo.model.Employee;
+import com.deloitte.springboot.demo.repository.EmployeeRepository;
 
 @Service
 public class EmployeeService {
 
-	public Employee getEmployeeById(int employeeId) {
-		System.out.println(employeeId);
-		Employee emp = new Employee(101, "Sonu", 90000);
+	@Autowired
+	EmployeeRepository empRepository;
+
+	public Employee getEmployeeById(int employeeId) { // 101
+		System.out.println("getEmployeeById " + employeeId);
+		Optional<Employee> empOptional = empRepository.findById(employeeId);
+		Employee emp = empOptional.get();
 		return emp;
 	}
 
 	public List<Employee> getAllEmployees() {
-		List<Employee> empList = new ArrayList<>();
-		empList.add(new Employee(101, "Sonu", 90000));
-		empList.add(new Employee(102, "Monu", 95000));
-		empList.add(new Employee(103, "Tonu", 85000));
-		System.out.println(empList.size());
-		return empList;
+		System.out.println("getAllEmployees");
+		return empRepository.findAll();
 	}
 
 //	addEmployee()
