ce46c540d9e7db3779628a9c26783fed39f452eb
Vaman Deshmukh <vamandeshmukh@gmail.com>
2022-10-17 16:06:21 +0530

    spring-boot

--- src/main/java/com/deloitte/springboot/demo/model/Department.java
+++ src/main/java/com/deloitte/springboot/demo/model/Department.java
@@ -7,4 +7,6 @@ public class Department {
 	private String city;
 
 	// constructors, getters-setters, toString etc
+	
+	// create DepartmentController, DepartmentService, DepartmentRepository etc to perform CRUD Ops 
 }
