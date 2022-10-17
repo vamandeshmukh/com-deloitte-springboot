d89ea19cc1c3ab6630ef310f662c2cfe35bc6fa4
Vaman Deshmukh <vamandeshmukh@gmail.com>
2022-10-17 11:36:50 +0530

    spring-boot

--- src/main/java/com/deloitte/springboot/demo/controller/EmployeeController.java
+++ src/main/java/com/deloitte/springboot/demo/controller/EmployeeController.java
@@ -3,6 +3,7 @@ package com.deloitte.springboot.demo.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +13,9 @@ import com.deloitte.springboot.demo.service.EmployeeService;
 @RestController
 public class EmployeeController {
 
+//	EmployeeService empService = new EmployeeService();
+
+	@Autowired
 	EmployeeService empService;
 
 //	http://localhost:8082/get-emp-by-id
