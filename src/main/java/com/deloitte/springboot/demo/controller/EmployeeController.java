afbe75482fdc1296d5c05d27195ed64c298ad513
Vaman Deshmukh <vamandeshmukh@gmail.com>
2022-10-17 11:41:21 +0530

    spring-boot

--- src/main/java/com/deloitte/springboot/demo/controller/EmployeeController.java
+++ src/main/java/com/deloitte/springboot/demo/controller/EmployeeController.java
@@ -1,6 +1,5 @@
 package com.deloitte.springboot.demo.controller;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,16 +24,11 @@ public class EmployeeController {
 		return empService.getEmployeeById(101);
 	}
 
-//	http://localhost:8082/emp-list
+//	http://localhost:8082/get-alll-emps
 
-	@GetMapping("/emp-list")
-	public List<Employee> getEmpList() {
-		List<Employee> empList = new ArrayList<>();
-		empList.add(new Employee(101, "Sonu", 90000));
-		empList.add(new Employee(102, "Monu", 95000));
-		empList.add(new Employee(103, "Tonu", 85000));
-		System.out.println(empList.size());
-		return empList;
+	@GetMapping("/get-alll-emps")
+	public List<Employee> getAllEmps() {
+		return empService.getAllEmployees();
 	}
 
 	// create other methods
--- src/main/java/com/deloitte/springboot/demo/service/EmployeeService.java
+++ src/main/java/com/deloitte/springboot/demo/service/EmployeeService.java
@@ -1,5 +1,8 @@
 package com.deloitte.springboot.demo.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 import com.deloitte.springboot.demo.model.Employee;
@@ -10,8 +13,15 @@ public class EmployeeService {
 	public Employee getEmployeeById(int employeeId) {
 		System.out.println(employeeId);
 		Employee emp = new Employee(101, "Sonu", 90000);
-		System.out.println(emp.toString());
 		return emp;
 	}
 
+	public List<Employee> getAllEmployees() {
+		List<Employee> empList = new ArrayList<>();
+		empList.add(new Employee(101, "Sonu", 90000));
+		empList.add(new Employee(102, "Monu", 95000));
+		empList.add(new Employee(103, "Tonu", 85000));
+		System.out.println(empList.size());
+		return empList;
+	}
 }
