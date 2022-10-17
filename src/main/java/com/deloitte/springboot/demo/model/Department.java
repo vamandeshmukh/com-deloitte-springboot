f7c68da68913e45ec03b1c208d2cf6834525723a
Vaman Deshmukh <vamandeshmukh@gmail.com>
2022-10-17 16:06:08 +0530

    spring-boot

---
+++ src/main/java/com/deloitte/springboot/demo/model/Department.java
@@ -0,0 +1,10 @@
+package com.deloitte.springboot.demo.model;
+
+public class Department {
+
+	private int departmentId;
+	private String departmentName;
+	private String city;
+
+	// constructors, getters-setters, toString etc
+}
