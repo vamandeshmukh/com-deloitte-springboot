5f3151ec1ed6dbadb9cad61e1a693ddb0749fae6
Vaman Deshmukh <vamandeshmukh@gmail.com>
2022-10-19 12:56:16 +0530

    spring-boot

--- src/main/java/com/deloitte/springboot/demo/App.java
+++ src/main/java/com/deloitte/springboot/demo/App.java
@@ -12,10 +12,7 @@ public class App {
 
 	public static void main(String[] args) {
 		LOG.info("Starting");
-		System.out.println("Starting...");
 		SpringApplication.run(App.class, args);
 		LOG.info("Started");
-		System.out.println("Started...");
 	}
-
 }
