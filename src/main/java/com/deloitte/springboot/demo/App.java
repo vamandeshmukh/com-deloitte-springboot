91383b080da3049d287637fcca9422d51c0390a6
Vaman Deshmukh <vamandeshmukh@gmail.com>
2022-10-19 09:56:39 +0530

    spring-boot

--- src/main/java/com/deloitte/springboot/demo/App.java
+++ src/main/java/com/deloitte/springboot/demo/App.java
@@ -1,17 +1,21 @@
 package com.deloitte.springboot.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.stereotype.Component;
 
-
-//@ComponentScan
 @SpringBootApplication
 public class App {
 
+	private static final Logger LOG = LoggerFactory.getLogger(App.class);
+
 	public static void main(String[] args) {
+		LOG.info("Starting");
+		System.out.println("Starting...");
 		SpringApplication.run(App.class, args);
+		LOG.info("Started");
+		System.out.println("Started...");
 	}
 
 }
